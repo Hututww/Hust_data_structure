63f4cf3e053e511d07099376aa1204faed090459
饭斗花园土土大王 <stevechen2005@outlook.com>
2026-05-10 14:30:13 +0800

    手不算生 好事情😸

---
+++ 1_顺序表与链表/顺序表求josephus问题.cpp
@@ -0,0 +1,30 @@
+# include <stdio.h>
+
+int main(){
+    int number = 5;
+    int start = 0;
+    int distance = 2;
+    int removed = 0;
+
+    int a[number];
+    for (int i = 0; i < number; i++){
+        a[i] = 1;
+    }
+
+    for (int j = start; removed < number - 1;){
+        j += distance;
+        a[j] = 0;
+        removed++;
+
+        if (j > number) j -=number;
+        while (a[j] = 0) j++;
+    }
+
+    for (int k = 0; k < number; k++){
+        if(a[k] != 0){
+            printf("final survivor index: %d\n", k);
+            break;
+        }
+    }
+    return 0;
+}
